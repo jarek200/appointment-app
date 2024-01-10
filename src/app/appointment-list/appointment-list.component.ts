20fd3abfe4eb47c06032fb32fd5aaf28d6f9af04
Jarek Wyprzal <jwyprzal@gmail.com>
2024-01-10 10:44:52 +0000

    first commit

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css'],
+})
+export class AppointmentListComponent {
+  appointment: Appointment[] = [];
+}
